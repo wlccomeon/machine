52d16ceeb6c673e8b5c4b6d11c5f8b9869ce17d8
mac <yimiyami>
2018-02-04 17:40:25 +0800

    测试knn算法

---
+++ Ch02/testKNN.py
@@ -0,0 +1,24 @@
+import kNN
+
+group,labels = kNN.createDataSet()
+
+from numpy import *
+import operator
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+if __name__ == '__main__':
+    a = classify0([0,0],group,labels,3)
+    print (a)
